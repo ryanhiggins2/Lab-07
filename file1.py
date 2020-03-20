cd7733badd89b11b70d5b8ed71b331058e27ab70
ryan <myemail@gmail.com>
2020-03-20 15:57:08 -0400

    added file1.py

---
+++ file1.py
@@ -0,0 +1,43 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 23:03:10) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====================================================== RESTART: C:/Users/ryn_h/Desktop/Lab-07/file1.py =====================================================
+Hello World
+>>> 
+================ RESTART: C:/Users/ryn_h/Desktop/Lab-07/file1.py ===============
+Hello World
+Hello World, my name is Ryan and I love Python!
+>>> 
+================ RESTART: C:/Users/ryn_h/Desktop/Lab-07/file1.py ===============
+Hello World
+Hello World, my name is Ryan and I love Python!
+Hello! Please enter your name!
+================ RESTART: C:/Users/ryn_h/Desktop/Lab-07/file1.py ===============
+Hello World
+Hello World, my name is Ryan and I love Python!
+Hello! Please enter your name!
+================ RESTART: C:/Users/ryn_h/Desktop/Lab-07/file1.py ===============
+Hello! Please enter your name!Ryan 
+Hello Ryan  nice to meet you
+>>> 
+================ RESTART: C:/Users/ryn_h/Desktop/Lab-07/file1.py ===============
+Hello! Please enter your name!Ryan Higgins
+Hello Ryan Higgins nice to meet you
+>>> 
+================ RESTART: C:/Users/ryn_h/Desktop/Lab-07/file1.py ===============
+Traceback (most recent call last):
+  File "C:/Users/ryn_h/Desktop/Lab-07/file1.py", line 1, in <module>
+    if (userName.lower() == "ryan" and userPassword == "CSF534!"):
+NameError: name 'userName' is not defined
+>>> 
+================ RESTART: C:/Users/ryn_h/Desktop/Lab-07/file1.py ===============
+Traceback (most recent call last):
+  File "C:/Users/ryn_h/Desktop/Lab-07/file1.py", line 1, in <module>
+    if (userName.lower() == "ryan" and userPassword == "CSF534!"):
+NameError: name 'userName' is not defined
+>>> 
+================ RESTART: C:/Users/ryn_h/Desktop/Lab-07/file1.py ===============
+Hello! Please enter your facebook username!ryan
+Hello! Please enter your facebook password!CSF534!
+Welcome to Facebook!
+>>> 
